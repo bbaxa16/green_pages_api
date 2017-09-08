9e3cb3b993261c4d4a2f9a8a2a0304fdd5f07a19
Susan <sungjamesjoo@gmail.com>
2017-09-08 12:49:50 -0400

    Master merge

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
       token = create_token(user.id, user.username)
       render json: {status: 200, token: token, user: user}
     else
-      render json: {stauts: 401, message: "Unauthorized"}
+      render json: {status: 401, message: "Unauthorized"}
     end
   end
 
