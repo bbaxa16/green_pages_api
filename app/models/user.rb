44354bcf284cb9b10ef5d93312316b6ce4a4014f
Susan <sungjamesjoo@gmail.com>
2017-09-08 13:31:27 -0400

    Case sensitive false in user.rb model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true, case_sensitive: false
   validates :password, presence: true
   has_secure_password
 end
