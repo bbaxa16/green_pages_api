307ac00a9ed6a148140d7c3c13e4f55f2c55fbd0
Susan <sungjamesjoo@gmail.com>
2017-09-08 17:56:45 -0400

    Decoding token and checking current user works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,13 +18,12 @@ class UsersController < ApplicationController
   # GET /users
   def index
     @users = User.all
-
     render json: @users
   end
 
   # GET /users/1
   def show
-    render json: @user
+    render json: get_current_user
   end
 
   # POST /users
