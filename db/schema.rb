226d19a10713267693f3c4ee9a7f9731613c7110
Susan <sungjamesjoo@gmail.com>
2017-09-11 17:46:21 -0400

    Created stuff for the relational databases

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170911204425) do
+ActiveRecord::Schema.define(version: 20170911213957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ledgers", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "strain_id"
+    t.integer "qty"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["strain_id"], name: "index_ledgers_on_strain_id"
+    t.index ["user_id"], name: "index_ledgers_on_user_id"
+  end
 
   create_table "strains", force: :cascade do |t|
     t.string "name"
@@ -29,6 +39,9 @@ ActiveRecord::Schema.define(version: 20170911204425) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "favorites", default: [], array: true
   end
 
+  add_foreign_key "ledgers", "strains"
+  add_foreign_key "ledgers", "users"
 end
